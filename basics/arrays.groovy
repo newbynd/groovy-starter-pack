22b63070fd19caa249357cb6b372bdc7f9f2809e
newbynd <nestorromero.contacto@gmail.com>
2020-08-18 21:51:03 +0200

    adding methods and utilities + maps

--- basics/arrays.groovy
+++ basics/arrays.groovy
@@ -6,10 +6,21 @@ assert arrayOfNumbers.size() == 3
 //ACCESSING ELEMENTS
 assert arrayOfNumbers[1] == 40
 assert arrayOfNumbers[-1] == 60
+assert arrayOfNumbers.getAt(0) == 20
+//__ get a range
+assert arrayOfNumbers.getAt(0..1) == [20, 40]
 
 //MODIFYNG ELEMENTS
 arrayOfNumbers[1] = 41
 assert arrayOfNumbers[1] == 41
+
+//ADDING ELEMENTS
+arrayOfNumbers + 400
+assert arrayOfNumbers.size() == 4
+
+//DELETING ELEMENTS
+arrayOfNumbers + arrayOfNumbers[-1]
+assert arrayOfNumbers.size() == 3
 
 //SUM OF ALL NUMBERS
 assert arrayOfNumbers.sum() == 121
--- basics/lists.groovy
+++ basics/lists.groovy
@@ -1,3 +1,8 @@
+//DOCS
+//Groovy: https://groovy-lang.org/syntax.html
+//LinkedList: https://docs.oracle.com/javase/7/docs/api/java/util/LinkedList.html
+//ArrayList: https://docs.oracle.com/javase/7/docs/api/java/util/ArrayList.html
+
 def listOfNumbers = [24, 900]
 assert listOfNumbers.size() == 2
 assert listOfNumbers instanceof java.util.List
@@ -15,25 +20,41 @@ assert linkedListOfNumbers instanceof java.util.List
 assert linkedListOfNumbers instanceof java.util.LinkedList
 
 //ACCESSING ELEMENTS
-def listOfNumbers2 = [24, 900, 42, 50000]
+def listOfNumbers2 = [24, 900, 42, 50000, 50000]
 assert listOfNumbers2[0] == 24
 assert listOfNumbers2[-1] == 50000
 assert listOfNumbers2.getAt(0) == 24
+//__ get a range
 assert listOfNumbers2.getAt(0..1) == [24, 900]
+//__ get all unique data
+assert listOfNumbers2.unique() == [24, 900, 42, 50000]
 
 //MODIFYING ELEMENTS
 listOfNumbers2[0] = 24000
 assert listOfNumbers2[0] == 24000
-
+listOfNumbers2.putAt(0, 1)
+assert listOfNumbers2[0] == 1
 //ADDING ELEMENTS
+//__ add element at the end of the list
 listOfNumbers2 << 29
 assert listOfNumbers2.size() == 5
+//__ add element at the start of the list
+listOfNumbers2.push(400)
+assert listOfNumbers2.size() == 6
+
+//DELETING ELEMENTS
+//__ delete an element at the start of the list
+listOfNumbers2.pop()
+assert listOfNumbers2.size() == 5
+
 
 //LISTS CAN CONTAIN ANOTHER LISTS
 def listOfLists = [["hi, i'm a list", 3], ["me too", true]]
 assert listOfLists[0][0] == "hi, i'm a list"
 assert listOfLists[1][0] == "me too"
 assert listOfLists[1][1] == true
+//FLATTEN TO CONVERT 2 DIMENSIONAL ARRAY IN ONE DIMENSIONAL ARRAY
+assert listOfLists.flatten() == ['hi, i\'m a list', 3, 'me too', true]
 
 //REVERSE LISTS
 def listToReverse = [1, 2, 3]
